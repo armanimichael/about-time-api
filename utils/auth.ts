ec7038d550fd59a889d0102552c2891caba3dc57
Michael Armani <michaelarmani00@gmail.com>
2020-11-03 16:49:34 +0100

    Added promise-driven email validation from callback email-verifier

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,5 +1,12 @@
 import { Request } from 'express';
+import EmailVerifier from 'email-verifier';
 import { Types } from 'mongoose';
+
+interface EmailValidation {
+  smtpCheck: string | boolean;
+  dnsCheck: string | boolean;
+  disposableCheck: string | boolean;
+}
 
 const getLoggedUserID = (req: Request): Types.ObjectId => {
   interface SecureRouteRequest extends Request {
@@ -12,4 +19,20 @@ const getLoggedUserID = (req: Request): Types.ObjectId => {
   return (req as SecureRouteRequest).user._id;
 };
 
-export { getLoggedUserID };
+const validateEmail = (email: string): Promise<EmailValidation> => {
+  const mailVerifier = new EmailVerifier(process.env.EMAIL_VERIFICATION_KEY, {
+    validateDNS: true,
+    checkDisposable: true,
+    validateSMTP: true,
+    checkCatchAll: false,
+    checkFree: false,
+  });
+
+  return new Promise((resolve, reject) => {
+    mailVerifier.verify(email, (err: unknown, data: EmailValidation) => {
+      err ? reject(err) : resolve(data);
+    });
+  });
+};
+
+export { getLoggedUserID, validateEmail };
